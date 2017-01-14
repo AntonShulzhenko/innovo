76ca75363442ea31cfa48cb47f4b35b9f8b30719
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2017-01-14 14:50:33 +0200

    about section

--- src/js/plugins/init-plugins.js
+++ src/js/plugins/init-plugins.js
@@ -3,6 +3,7 @@
     items: 1,
     center: true,
     nav: true,
+    navText: ['<i class="ion-ios-arrow-left"></i>', '<i class="ion-ios-arrow-right"></i>'],
     lazyLoad: true,
     smartSpeed: 400
   });
