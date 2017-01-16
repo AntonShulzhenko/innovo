0b56b087df06c33f7e4f7ee81dbd6aa0239c791a
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2017-01-16 03:15:17 +0200

    menu

--- src/js/plugins/nav.js
+++ src/js/plugins/nav.js
@@ -1,8 +1,29 @@
 (function() {
   const nav = document.querySelector('.nav');
-  const btn = document.querySelector('.contacts__item_menu');
+  const btn = document.querySelector('.menu-btn');
+  const top = document.querySelector('.top');
 
-  btn.addEventListener('click', () => {
-    nav.style.display = 'flex';
+  function toggleClass(el, className) {
+    if(!el) return;
+    if(!el.classList.contains(className)) {
+      disableScroll();
+    } else {
+      enableScroll();
+    }
+    el.classList.toggle(className);
+  }
+
+  nav.addEventListener('click', (e) => {
+    if(e.target.classList.contains('nav__link')) {
+      toggleClass(nav, 'is-active');
+      toggleClass(btn, 'is-active');
+      toggleClass(top, 'is-active');
+    }
+  });
+
+  btn.addEventListener('click', (e) => {
+    toggleClass(nav, 'is-active');
+    toggleClass(btn, 'is-active');
+    toggleClass(top, 'is-active');
   });
 }());
